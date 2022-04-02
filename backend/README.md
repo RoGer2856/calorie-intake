b8d34d1dc765629763eb4a05e82ca22269486e5e
Gergo Roth <roth@bytifex.com>
2022-04-02 06:59:34 +0200

    backend * ref: renamed diet-backend to diet-server * feat: created diet-jwt-manager that is a command line interface around DietAuthorization, can be used for creating jwt tokens * feat: adjusted README.md

--- backend/README.md
+++ backend/README.md
@@ -2,22 +2,35 @@
 
 This project is using cargo as its build system. It can be best installed with rustup. Please follow the instructions on its web page [https://rustup.rs/](https://rustup.rs/).
 
+The project contains a server (**diet-server**) and a jwt manager (**diet-jwt-manager**).
+
 ## Testing
 
 The project contains automatic tests. Please run `cargo test --all` to perform all tests.
 
-## Running
+## Running **diet-server**
 
-The project can be run in debug and release mode.
+For debug mode, run `cargo run --bin diet-server -- -h`
 
-For debug mode, run `cargo run`.
+For release mode, run `cargo run --bin diet-server --release -- -h`
 
-For release mode, run `cargo run --release`.
+Examples
+* Set log level to debug and listen on [::1]:3001: `RUST_LOG=debug cargo run --bin diet-server -- --listener-address [::1]:3001`
+
+## Running **diet-jwt-manager**
+
+For debug mode, run `cargo run --bin diet-jwt-manager -- -h`
+
+For release mode, run `cargo run --bin diet-jwt-manager --release -- -h`
+
+Examples:
+* Creating a new regular user jwt for diet-jwt-manager: `cargo run --bin diet-jwt-manager -- create -s test_resources/test_secrets.toml -r regular_user -u john`
+* Creating a new admin user jwt for diet-jwt-manager: `cargo run --bin diet-jwt-manager -- create -s test_resources/test_secrets.toml -r admin -u john`
 
 ## Building
 
 The project can be built in debug and release mode.
 
-For debug mode, run `cargo build`.
+For debug mode, run `cargo build`
 
-For release mode, run `cargo build --release`.
+For release mode, run `cargo build --release`
---
+++ backend/src/bin/diet-jwt-manager.rs
@@ -0,0 +1,63 @@
+extern crate diet;
+
+use std::str::FromStr;
+
+pub fn main() {
+    let matches = clap::Command::new("diet-jwt-manager")
+        .version("0.1.0")
+        .author("Gergő Róth <roth@bytifex.com>")
+        .about("With this tool one can manage JWT tokens for the diet-app")
+        .subcommand_required(true)
+        .subcommand(
+            clap::Command::new("create")
+                .about("Creates a new JWT")
+                .arg(
+                    clap::Arg::new("secret-file")
+                        .short('s')
+                        .long("secret-file")
+                        .value_name("FILE")
+                        .help("Reads the secrets from the given config file (toml)")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    clap::Arg::new("role")
+                        .short('r')
+                        .long("role")
+                        .value_name("ROLE")
+                        .help("Sets the role for the JWT.")
+                        .possible_value("admin")
+                        .possible_value("regular_user")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    clap::Arg::new("username")
+                        .short('u')
+                        .long("username")
+                        .value_name("USERNAME")
+                        .help("Sets the username for the JWT")
+                        .required(true)
+                        .takes_value(true),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("create") {
+        let secrets_file_location = matches.value_of("secret-file").unwrap();
+        let role = matches.value_of("role").unwrap();
+        let username = matches.value_of("username").unwrap();
+
+        let diet_authorization =
+            diet::services::DietAuthorization::new(secrets_file_location.into()).unwrap();
+
+        let jwt = diet_authorization
+            .create_jwt(
+                username.to_string(),
+                diet::services::RoleType::from_str(role).unwrap(),
+            )
+            .unwrap();
+
+        println!("{}", jwt);
+    }
+}
